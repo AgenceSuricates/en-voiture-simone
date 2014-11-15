bee899517867798a28b4967b9deb3a002740a5c6
Agence Suricates <agence.suricates@gmail.com>
2014-11-15 18:19:44 +0100

    Création de fichier + squelette
    Création du squelette + upload de cartons et images  + onglets

--- index.php
+++ index.php
@@ -1,1 +1,75 @@
-testelvis.php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <title>En Voiture Simone - Les défis qui dépassent les bornes !</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="En Voiture Simone"/>
+    <link rel="stylesheet" type="text/css" href="style/style.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Playball|Cookie' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="img/autres/voiture.png"/>
+</head>
+
+<body>
+   
+   <!-- MENU -->
+   
+   <div class="header">
+        <div class="logo"></div>
+        <div class="navigation"></div>
+        <div class="sociaux">
+   </div>
+   
+
+
+
+
+   <!-- SLOGAN -->
+
+   <div class="slogan">
+        <h1>Les défis qui dépassent les bornes !</h1>
+    </div>
+
+
+
+
+
+
+    <!-- PARTAGE RESEAUX SOCIAUX -->
+    <div class="partage">
+        <div class="facebook"></div>
+        <div class="twitter"></div>
+    </div>
+
+
+
+
+
+
+    <!-- PHRASE DE PRESENTATION -->
+
+    <div class="presentation"></div>
+
+
+
+
+
+    <!-- COMPTEUR DE DEFIS ENVOYE  -->
+
+    <div class="container_defis">    
+        <div class="compteur_defis"></div>
+    </div>
+
+
+
+
+
+    <!-- FOOTER -->
+
+    <div class="footer">
+        <div class="mentions_legales"></div>
+    </div>
+
+
+</body>
+</html>
