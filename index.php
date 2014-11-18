a1907c7648ba0ae9a158c724ffb488e74b29b719
AgenceSuricates <agence.suricates@gmail.com>
2014-11-18 23:46:54 +0100

    Home slider

--- index.php
+++ index.php
@@ -8,6 +8,22 @@
     <link rel="stylesheet" type="text/css" href="style/style.css"/>
     <link href='http://fonts.googleapis.com/css?family=Playball|Cookie' rel='stylesheet' type='text/css'>
     <link rel="shortcut icon" href="img/autres/voiture.png"/>
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <script src="js/modernizr.custom.63321.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.stackslider.js"></script>
+        <script type="text/javascript">
+            
+            $( function() {
+                
+                $( '#st-stack' ).stackslider();
+
+            });
+
+        </script>
+    <script type="text/javascript">
+    function ouvre(fichier) {ff=window.open(fichier,"popup","width=700,height=500,left=100,top=100") }
+    </script>
 </head>
 
 <body>
@@ -18,14 +34,16 @@
    <!-- MENU -->
    
    <div id="header">
-        <div class="logo"><img src="img/logo-evs.png" width="153" height="95" alt="En Voiture Simone"/></div>
+        <div class="logo"><img src="img/logo-evs.png" alt="En Voiture Simone"/></div>
         <div class="navigation">
             <ul>
                 <li><a href="#">LES DÉFIS</a></li>
                 <li><a href="#">LES CHALLENGERS</a></li>
+                <li><a href="#">JEUX CONCOURS</a></li>
             </ul>
         </div>
         <div class="sociaux">
+          
                 <a class="facebook" target="_blank" href="https://www.facebook.com/pages/En-Voiture-Simone/1488379481451366?notif_t=page_new_likes"></a>
                 <a class="twitter" target="_blank"href="https://twitter.com/EnVoitureSim0ne"></a>
                 <a class="instagram" target="_blank"href="http://instagram.com/en_voituresimone"></a>
@@ -38,33 +56,72 @@
    <!-- SLOGAN -->
 
    <div class="slogan">
-        <h1>Les défis qui dépassent les bornes !</h1>
+        <h1>Les défis qui dépassent <span class="font">les bornes !</span></h1>
     </div>
 
 
     <!-- SLIDER -->
-    <div id="slider">
-        <img src="img/cartons/DEFI_5.jpg" alt="Défi n°5 - En Voiture Simone"/>
-    </div>
 
+  
+        <div id="slider">
+                <ul id="st-stack" class="st-stack-raw">
+                   <li><div class="st-item"><a href="#"><img src="images/DEFI_1.jpg"/></a></div><div class="st-title"></div></li>
+
+                    <li><div class="st-item"><a href="#"><img src="images/DEFI_4.jpg"/></a></div><div class="st-title"></div></li>
+                    <li><div class="st-item"><a href="#"><img src="images/DEFI_5.jpg"/></a></div><div class="st-title"></div></li>
+                    <li><div class="st-item"><a href="#"><img src="images/DEFI_6.jpg"/></a></div><div class="st-title"></div></li>
+                    <li><div class="st-item"><a href="#"><img src="images/DEFI_9.jpg"/></a></div><div class="st-title"></div></li>
+                    
+                </ul>
+     </div>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.stackslider.js"></script>
+        <script type="text/javascript">
+            
+            $( function() {
+                
+                $( '#st-stack' ).stackslider();
+
+            });
+
+        </script>
 
 
     <!-- PARTAGE RESEAUX SOCIAUX -->
     <div id="partage">
-        <a class="btn-facebook" target="_blank" href=""><img src="img/btn-fb.png"/></a>
-        <a class="btn-twitter" target="_blank" href=""><img src="img/btn-twt.png"/></a>
+          <script>function fbs_click() {
+                u=location.href;
+                t=document.title;
+                window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
+                return false;
+                }
+            </script>
+            <a title="Partager sur Facebook" href="http://www.facebook.com/share.php?u=<url>" onclick="return fbs_click()" target="_blank">
+            <img src="img/btn-fb.png" alt="Partager sur Facebook" /></a>
+
+            <!--////////////////////-->
+          <script>function twt_click() {
+                u=location.href;
+                t=document.title;
+                window.open('http://www.twitter.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
+                return false;
+                }
+            </script>
+        
+         <a title="Partager sur Facebook" href="http://www.twitter.com/share.php?u=<url>" onclick="return twt_click()" target="_blank">
+            <img src="img/btn-twt.png" alt="Partager sur Twitter" /></a>
+        <!--////////////////////-->
+        <a class="btn-mail" target="_blank" href=""><img src="img/btn-mail.png"/></a>
     </div>
 
-
-
-
+    <!-- JS PARTAGE RESEAUX SOCIAUX -->
 
 
     <!-- PHRASE DE PRESENTATION -->
 
     <div class="presentation">
         
-        <h2>Défie tes grands-parents, ils pourraient te surprendre !</h2>
+        <h2>Défie tes grands-parents, ils pourraient <span class="font">te surprendre !</span></h2>
     </div>
 
 
@@ -85,8 +142,9 @@
 
     <div id="footer">
        <ul>
-            <li><a href="#">En Voiture Simone2014</a></li>
+            <li><a href="#">En Voiture Simone 2014</a></li>
             <li><a href="#">Créé par Agence Suricates</a></li> 
+            <li><a href="#">contact@en-voituresimone.fr</a></li> 
             <li><a href="#">Mentions Légales</a></li>
        </ul>
 
